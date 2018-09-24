faa7f056b7b4c1b9bc28ec336b33da4827c25e1a
unknown <victorst79@gmail.com>
2018-09-24 23:56:35 +0200

    Worksheet#5

--- Unit1/Worksheet#5/index.js
+++ Unit1/Worksheet#5/index.js
@@ -1,89 +1,169 @@
-// document.write("<h1>Worksheet#5</h1>");
+document.write("<h1>Worksheet#5</h1>");
 
-// document.write("<h3>Exercise 10</h3>");
+document.write("<h3>Exercise 10</h3>");
 
-// for(let i = 1; i <= 6; i++){
-//     document.write("<h"+i+">Header h"+i+"</h"+i+">");
-// }
+for(let i = 1; i <= 6; i++){
+    document.write("<h"+i+">Header h"+i+"</h"+i+">");
+}
 
-// document.write("<h3>Exercise 11</h3>");
+document.write("<h3>Exercise 11</h3>");
 
-// var n_col = prompt("Column Number");
-// var height = prompt("Height in px");
-// var width = prompt("width in px");
+var n_col = prompt("Column Number");
+var height = prompt("Height in px");
+var width = prompt("width in px");
 
-// document.write("<table cellspacing='2' border='1px solid black'>");
-// document.write("<tr height='" + height + "'>");    
-// for(let i = 0; i <= n_col - 1; i++){
-//     document.write("<td width='" + width + "'>a &nbsp</td>");    
-// }
-// document.write("</tr>");    
-// document.write("</table>");
+document.write("<table cellspacing='2' border='1px solid black'>");
+document.write("<tr height='" + height + "'>");    
+for(let i = 0; i <= n_col - 1; i++){
+    document.write("<td width='" + width + "'>a &nbsp</td>");    
+}
+document.write("</tr>");    
+document.write("</table>");
 
-// document.write("<h3>Exercise 12</h3>");
+document.write("<h3>Exercise 12</h3>");
 
-// var n_col = prompt("Column Number");
-// var height = prompt("Height in px");
-// var width = prompt("width in px");
+var n_col = prompt("Column Number");
+var height = prompt("Height in px");
+var width = prompt("width in px");
 
-// document.write("<table cellspacing='2' border='1px solid black'>");
-// document.write("<tr height='" + height + "'>");    
-// for(let i = 0; i <= n_col - 1; i++){
-//     if(i%2 == 0){
-//         document.write("<td bgcolor='white' width='" + width + "'>&nbsp</td>");
-//     }else{
-//         document.write("<td bgcolor='black' width='" + width + "'>&nbsp</td>");
-//     }
-// }
-// document.write("</tr>");    
-// document.write("</table>");
+document.write("<table cellspacing='2' border='1px solid black'>");
+document.write("<tr height='" + height + "'>");    
+for(let i = 0; i <= n_col - 1; i++){
+    if(i%2 == 0){
+        document.write("<td bgcolor='white' width='" + width + "'>&nbsp</td>");
+    }else{
+        document.write("<td bgcolor='black' width='" + width + "'>&nbsp</td>");
+    }
+}
+document.write("</tr>");    
+document.write("</table>");
 
-// document.write("<h3>Exercise 13</h3>");
+document.write("<h3>Exercise 13</h3>");
 
-// var n_col = prompt("Column Number");
-// var height = prompt("Height in px");
-// var width = prompt("width in px");
-// var i = 1;
-// document.write("<table cellspacing='2' border='1px solid black'>");
-// document.write("<tr height='" + height + "'>");
-// while(i <= n_col){
-//     document.write("<td width='" + width + "'>a &nbsp</td>");
-//     i++;
-// }
-// document.write("</tr>");    
-// document.write("</table>");
+var n_col = prompt("Column Number");
+var height = prompt("Height in px");
+var width = prompt("width in px");
+var i = 1;
+document.write("<table cellspacing='2' border='1px solid black'>");
+document.write("<tr height='" + height + "'>");
+while(i <= n_col){
+    document.write("<td width='" + width + "'>a &nbsp</td>");
+    i++;
+}
+document.write("</tr>");    
+document.write("</table>");
 
-// document.write("<h3>Exercise 14</h3>");
+document.write("<h3>Exercise 14</h3>");
 
-// var n_col = prompt("Column Number");
-// var height = prompt("Height in px");
-// var width = prompt("width in px");
-// var j = 1;
-// document.write("<table cellspacing='2' border='1px solid black'>");
-// document.write("<tr height='" + height + "'>");
-// while(j <= n_col){
-//     if(j%2 == 0){
-//         document.write("<td bgcolor='white' width='" + width + "'>&nbsp</td>");
-//     }else{
-//         document.write("<td bgcolor='black' width='" + width + "'>&nbsp</td>");
-//     }
-//     j++;
-// }
-// document.write("</tr>");    
-// document.write("</table>");
+var n_col = prompt("Column Number");
+var height = prompt("Height in px");
+var width = prompt("width in px");
+var j = 1;
+document.write("<table cellspacing='2' border='1px solid black'>");
+document.write("<tr height='" + height + "'>");
+while(j <= n_col){
+    if(j%2 == 0){
+        document.write("<td bgcolor='white' width='" + width + "'>&nbsp</td>");
+    }else{
+        document.write("<td bgcolor='black' width='" + width + "'>&nbsp</td>");
+    }
+    j++;
+}
+document.write("</tr>");    
+document.write("</table>");
 
 document.write("<h3>Exercise 15</h3>");
 
 var num_adivinar = prompt("Number to guess");
 var num = prompt("Number");
 
-while(num != num_adivinar){
-    if(num < num_adivinar){
-        num = prompt("Try again, the number you are looking for is greater.");
-    }else if(num > num_adivinar){
-        num = prompt("Try again, the number you are looking for is less.");
-    }else{
-        alert("Congratulations");
+if(num_adivinar != num){
+    while(num_adivinar != num){
+        if(num < num_adivinar){
+            alert("Try again, the number you are looking for is greater.");
+            num = prompt("Number");
+        }else if(num > num_adivinar){
+            alert("Try again, the number you are looking for is less.");
+            num = prompt("Number");
+        }
     }
+}else{
+    alert("Congratulations");
+    document.write("You win this game");
 }
 
+document.write("<h3>Exercise 16</h3>");
+
+var num_adivinar2 = prompt("Number to guess");
+var num2 = prompt("Number");
+
+if(num_adivinar2 != num2){
+    do{
+        if(num2 < num_adivinar2){
+            alert("Try again, the number you are looking for is greater.");
+            num2 = prompt("Number");
+        }else if(num2 > num_adivinar2){
+            alert("Try again, the number you are looking for is less.");
+            num2 = prompt("Number");
+        }
+    }while(num_adivinar2 != num2);
+}else{
+    alert("Congratulations");
+    document.write("You win this game");
+}
+
+document.write("<h3>Exercise 17</h3>");
+
+for(let i = 0;i <= 10;i++){
+    document.write("<h2>Multiplication table of "+i+" </h2>");
+    for(let j = 0;j <= 10;j++){
+        let result = i*j;
+        document.write(i + " x " + j  + " = " + result + "<br>");
+    }
+    document.write("<br>");
+}
+
+document.write("<h3>Exercise 18</h3>");
+
+var n_col2 = prompt("Number of column");
+var n_row2 = prompt("Number of row");
+var height2 = prompt("Height in px");
+var width2 = prompt("Width in px");
+
+document.write("<table border='1px solid black' cellspading='2' bgcolor='black'>");
+for(let i = 1; i <= n_col2;i++){
+    document.write("<tr height='"+ height2 +"' bgcolor='white'");
+    for(let j = 1; j <= n_row2;j++){
+        document.write("<td width='"+ width2 +"'>&nbsp;</td>");
+    }
+    document.write("</tr>");
+}
+for(let i = 1; i <= n_col2;i++){
+    document.write("<tr height='"+ height2 +"' bgcolor='white'");
+    for(let j = 1; j <= n_row2;j++){
+        document.write("<td width='"+ width2 +"'>&nbsp;</td>");
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
+
+document.write("<h3>Exercise 19</h3>");
+
+var wah = prompt("Width and Height");
+var aux = 1;
+
+document.write("<table border='1px solid black' cellspading='2' bgcolor='black'>");
+for(let i = 0; i <= 8;i++){
+    document.write("<tr height='"+ wah +"' bgcolor='white'>");
+    for(let j = 0; j <= 8;j++){
+        if(aux == 1){
+            document.write("<td width='"+ wah +"' bgcolor='white'>");
+            aux = 0;
+        }else{
+            document.write("<td width='"+ wah +"' bgcolor='black'>");
+            aux = 1;
+        }
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
