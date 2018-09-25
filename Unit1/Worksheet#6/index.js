a258a45c9266269d1c4973bf21eb19317640abd7
unknown <victorst79@gmail.com>
2018-09-26 00:01:54 +0200

    Worksheet#6

--- Unit1/Worksheet#6/index.js
+++ Unit1/Worksheet#6/index.js
@@ -12,7 +12,7 @@ sumAll(...params);
 document.write("<h3>Exercise 2</h3>");
 
 function addOnlyNums(...arguments){
-    var sum = 0;
+    let sum = 0;
     for(let i = 0; i <= arguments.length-1;i++){
         if( typeof arguments[i] === 'number'){
             sum = sum+arguments[i];
@@ -25,3 +25,39 @@ function addOnlyNums(...arguments){
 
 addOnlyNums(5,5,5,2,6,9,8,10);
 
+document.write("<h3>Exercise 3</h3>");
+
+function countTheArgs(...args){
+    var cont = 0;
+    for(let i = 0;i <= arguments.length-1;i++){
+        cont++;
+    }
+    document.write(cont);
+    return cont;    
+}
+
+countTheArgs(1,2,'hi','again');
+
+document.write("<h3>Exercise 4</h3>");
+
+function combineTwoArrays(...params){
+    var aparam = [];
+    for(let i = 0; i <= arguments.length-1; i++){
+        aparam.push(arguments[i]);
+    }
+    return aparam;
+}
+
+console.log(combineTwoArrays([1,2,'hola',6],[21,true]));
+
+document.write("<h3>Exercise 5</h3>");
+
+function sumEveryOther(){
+
+}
+
+document.write("<h3>Exercise 6</h3>");
+
+function onlyUniques(){
+    
+}
