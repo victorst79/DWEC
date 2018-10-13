cd41497794c7ff60e7d3b8bc80d17c01b35c3015
unknown <victorst79@gmail.com>
2018-10-13 20:10:09 +0200

    TicTacToe class and methods function whitouth interface

---
+++ Unit 3/Worksheet#3/Tic-Tac-Toe/main.js
@@ -0,0 +1,33 @@
+class Tictactoe{
+    constructor(){
+        var board = [];
+
+        for(let i = 0; i < 3; i++){
+            board[i] = [i];
+            for(let j = 0; j < 3; j++){
+                board[i][j] = "Empty";
+            }
+        }
+
+        this.board = board;
+    }
+
+    // GETS
+    getBoard(){
+        return this.board;
+    }
+
+    // SETS
+    setBlackToken(line,column){
+        return this.board[line][column] = "Black";
+    }
+
+    setWhiteToken(line,column){
+        return this.board[line][column] = "White";
+    }    
+}
+
+
+
+var game = new Tictactoe();
+console.log(game);
