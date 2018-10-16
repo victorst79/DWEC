e9ed14b6047ab5c1a076bb244c4c88c99881e425
unknown <victorst79@gmail.com>
2018-10-16 14:29:59 +0200

    WS#1-Unit4 improvements in clear functions

--- Unit 4/main.js
+++ Unit 4/main.js
@@ -16,29 +16,36 @@ function positionMouse(event){
     document.getElementById("y").textContent = y;
 }
 
-// Exercise 4
+// Exercise 4 and 5
 function draw(){
     document.write("<table cellspacing='0' border='1px solid black'>");
     for(let i = 0; i <= 19; i++){
-        document.write("<tr>");
+        document.write("<tr height='20'>");
         for(let j = 0; j <= 19; j++){
-            document.write("<td onmouse='color(this);'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>");
+            document.write("<td id='square' onmousemove='color(event)' height='20' width='20'></td>");
         }
         document.write("</tr>");
     }    
     document.write("</table>");
+    document.write("<button onclick='deletColor()'>Clear</button>");
 }
 
-function color(square) {
-    if (event.shiftKey == true) {
-        this.style.backgroundColor = "blue";
-    }else if (event.ctrlKey == true) {
-        this.style.backgroundColor = "red";
+function color(event) {
+    if(event.shiftKey) {
+        event.target.style.backgroundColor = "blue";
+    }
+    if(event.ctrlKey) {
+        event.target.style.backgroundColor = "red";
+    }
+    if(event.altKey){
+        event.target.style.backgroundColor = "white";
     }
 }
 
-draw();
+function deletColor(){
+    var square = document.getElementById("square");
+}
 
-// Excercise 5
+draw();
 
 // Exercise 9
