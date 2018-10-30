f73614d6fcca46e418c125e01a9c7b4e6c0df8f1
unknown <victorst79@gmail.com>
2018-10-30 22:23:21 +0100

    Working tictactoe

---
+++ Unit 5/TicTacToe/tictactoe.js
@@ -0,0 +1,30 @@
+class Tictactoe{
+    constructor(){
+        var board = [];
+
+        for(let i = 0; i < 3; i++){
+            board[i] = [i];
+            for(let j = 0; j < 3; j++){
+                board[i][j] = "Empty";
+            }
+        }
+
+        this.board = board;
+    }
+
+    // GETS
+    getBoard(){
+        return this.board;
+    }
+
+    // SETS
+    setBlackToken(line,column){
+        console.log(this.getBoard());
+        return this.board[line][column] = "Black";
+    }
+
+    setWhiteToken(line,column){
+        console.log(this.getBoard());
+        return this.board[line][column] = "White";
+    }
+}
