afc56ffccef7e7f2cdd51a261775f72511456c2a
unknown <victorst79@gmail.com>
2018-10-31 13:33:11 +0100

    Improvement in tictactoe functions and modules

--- Unit 5/TicTacToe/main.js
+++ Unit 5/TicTacToe/main.js
@@ -1,14 +1,25 @@
 import {Tictactoe} from './tictactoe.js';
 
-var board = new Tictactoe;
+var board = new Tictactoe();
 // console.log(board.getBoard()[0][0]);
 
-function play(line,column){
-    if(board.getBoard()[line][column] == "Empty"){
-        console.log("CHANGE");
-    }else{
-        console.log("NOT EMPTY");
-    }
-}
+// function play(line,column){
+//     if(board.getBoard()[line][column] == "Empty"){
+//         console.log("CHANGE");
+//     }else{
+//         console.log("NOT EMPTY");
+//     }
+// }
 
 // document.getElementById("01").onclick = play(0,1);
+
+function play(){
+    var celdas = [...document.getElementsByTagName("td")]
+    celdas.map( (td) => td.onclick=evento);
+}
+
+function evento(event){
+    board.setBlackToken(event.target.id[0],event.target.id[1]);
+}
+
+play();
