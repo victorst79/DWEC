f8c20dd4469f09d29aa64ce55d4db7b79c8955da
unknown <victorst79@gmail.com>
2018-10-31 10:52:36 +0100

    DocumentsJS include in module tictactoe

--- Unit 5/TicTacToe/tictactoe.js
+++ Unit 5/TicTacToe/tictactoe.js
@@ -1,4 +1,7 @@
 class Tictactoe{
+    /**
+    * The constructor generates a bidimensional array with 3x3 positions, which simulates the logic of a game board.
+    */
     constructor(){
         var board = [];
 
@@ -13,16 +16,41 @@ class Tictactoe{
     }
 
     // GETS
+    
+    /**
+    * The method return array with all position and data of the TicTacToe board.
+    * 
+    * @return {Array} board
+    */
     getBoard(){
         return this.board;
     }
 
     // SETS
+
+    /**
+    * Receives 2 number, `line` is for a line position in the
+    * array, `colum` is for a column position.
+    * The method places a black tile in the chosen position.
+    * 
+    * Ex: setBlackToken(0,1); A black tile is placed in the position [0,1]
+    * @param {Number} line
+    * @param {Number} column
+    */
     setBlackToken(line,column){
         console.log(this.getBoard());
         return this.board[line][column] = "Black";
     }
 
+    /**
+    * Receives 2 number, `line` is for a line position in the
+    * array, `colum` is for a column position.
+    * The method places a white tile in the chosen position.
+    * 
+    * Ex: setBlackToken(0,1); A black tile is placed in the position [0,1]
+    * @param {Number} line
+    * @param {Number} column
+    */
     setWhiteToken(line,column){
         console.log(this.getBoard());
         return this.board[line][column] = "White";
