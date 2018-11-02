eafa1f5447d8496f0bd01d6aea57e873d956de5c
unknown <victorst79@gmail.com>
2018-11-02 22:10:01 +0100

    Finished tictactoe

--- Unit 5/TicTacToe/main.js
+++ Unit 5/TicTacToe/main.js
@@ -12,17 +12,19 @@ function event(event){
     if(board.getBoard()[event.target.id[0]][event.target.id[1]] == "Empty"){
         if(turn == true){
             board.setCrossToken(event.target.id[0],event.target.id[1]);
-            document.getElementById(event.target.id[0]+event.target.id[1]).style.backgroundColor = "red";
+            document.getElementById(event.target.id[0]+event.target.id[1]).classList.add("cross");
             turn = false;
             if(board.check() == true){
-                alert("Cross Win!");
+                document.getElementById("result").innerHTML = "Cross Win!";
+                document.getElementById("button").innerHTML = "<a href='index.html'>Reset Game</a>";
             }
         }else if(turn == false){
             board.setCircleToken(event.target.id[0],event.target.id[1]);
-            document.getElementById(event.target.id[0]+event.target.id[1]).style.backgroundColor = "green";
+            document.getElementById(event.target.id[0]+event.target.id[1]).classList.add("circle");
             turn = true;
             if(board.check() == false){
-                alert("Circle Win!");
+                document.getElementById("result").innerHTML = "Circle Win!";
+                document.getElementById("button").innerHTML = "<a href='index.html'>Reset Game</a>";
             }
         }
     }else{
