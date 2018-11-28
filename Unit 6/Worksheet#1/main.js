d993422f4b6f252e1e3758964468ef9d66472cc4
Victor Santaella <victorst79@gmail.com>
2018-11-28 12:52:53 +0100

    some changes in AJAX exercises

--- Unit 6/Worksheet#1/main.js
+++ Unit 6/Worksheet#1/main.js
@@ -1,22 +1,39 @@
-function mailBox(){
-	if (window.XMLHttpRequest) {
-    	var request = new XMLHttpRequest();
- 	} else {
-    	var request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    
-    request.open("POST", "mail.xml", true);
-    request.send();
-    
-    function newMail(){
-        if (request.readyState == 4 && request.status == 200) {
-            var xmlResult = request.responseXML;
-            var mail = xmlResult.getElementsByTagName("mail");
-            console.log(mail);
-        }
-    }
-}
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
 
-window.onload = function(){
-	mailBox();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var mails = xhttp.responseXML.documentElement.getElementsByTagName("mail");
+            var mailBox = document.getElementById("mail-box");
+            
+            console.log(xhttp.responseXML.documentElement.getElementsByTagName("mail")[0].children[0].innerHTML);
+            
+            // TO 
+            var to = document.createElement("p"); 
+            var toValue = document.createTextNode("To: "+mails[0].children[0].innerHTML);
+            document.getElementById("mail-box").appendChild(to);
+            to.appendChild(toValue);
+
+            // FROM 
+            var to = document.createElement("p"); 
+            var toValue = document.createTextNode("From: "+mails[0].children[1].innerHTML);
+            document.getElementById("mail-box").appendChild(to);
+            to.appendChild(toValue);
+
+            // HEADING 
+            var to = document.createElement("h3"); 
+            var toValue = document.createTextNode("Heading: "+mails[0].children[2].innerHTML);
+            document.getElementById("mail-box").appendChild(to);
+            to.appendChild(toValue);
+
+            // HEADING 
+            var to = document.createElement("p"); 
+            var toValue = document.createTextNode("Message: "+mails[0].children[3].innerHTML);
+            document.getElementById("mail-box").appendChild(to);
+            to.appendChild(toValue);
+        }
+    };
+
+    xhttp.open("GET", "./mail.xml", true);
+    xhttp.send();
 }
