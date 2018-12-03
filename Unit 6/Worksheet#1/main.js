af0da32e82af0ebad13cc198a78d4717d4e1906d
Victor Santaella <victorst79@gmail.com>
2018-12-03 14:30:51 +0100

    changes in interval ajax

--- Unit 6/Worksheet#1/main.js
+++ Unit 6/Worksheet#1/main.js
@@ -3,37 +3,40 @@ function loadDoc() {
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var mails = xhttp.responseXML.documentElement.getElementsByTagName("mail");
+            var mails = [...xhttp.responseXML.documentElement.getElementsByTagName("mail")];
             var mailBox = document.getElementById("mail-box");
             
-            console.log(xhttp.responseXML.documentElement.getElementsByTagName("mail")[0].children[0].innerHTML);
-            
-            // TO 
-            var to = document.createElement("p"); 
-            var toValue = document.createTextNode("To: "+mails[0].children[0].innerHTML);
-            document.getElementById("mail-box").appendChild(to);
-            to.appendChild(toValue);
+            for(let i = 0; i < mails.length; i++){
+                // TO 
+                var to = document.createElement("p"); 
+                var toValue = document.createTextNode("To: "+mails[i].children[0].innerHTML);
+                document.getElementById("mail-box").appendChild(to);
+                to.appendChild(toValue);
 
-            // FROM 
-            var to = document.createElement("p"); 
-            var toValue = document.createTextNode("From: "+mails[0].children[1].innerHTML);
-            document.getElementById("mail-box").appendChild(to);
-            to.appendChild(toValue);
+                // FROM 
+                var from = document.createElement("p"); 
+                var fromValue = document.createTextNode("From: "+mails[i].children[1].innerHTML);
+                document.getElementById("mail-box").appendChild(from);
+                from.appendChild(fromValue);
 
-            // HEADING 
-            var to = document.createElement("h3"); 
-            var toValue = document.createTextNode("Heading: "+mails[0].children[2].innerHTML);
-            document.getElementById("mail-box").appendChild(to);
-            to.appendChild(toValue);
+                // HEADING 
+                var heading = document.createElement("h3"); 
+                var headingValue = document.createTextNode("Heading: "+mails[i].children[2].innerHTML);
+                document.getElementById("mail-box").appendChild(heading);
+                heading.appendChild(headingValue);
 
-            // HEADING 
-            var to = document.createElement("p"); 
-            var toValue = document.createTextNode("Message: "+mails[0].children[3].innerHTML);
-            document.getElementById("mail-box").appendChild(to);
-            to.appendChild(toValue);
+                // MESSAGE 
+                var message = document.createElement("p"); 
+                var messageValue = document.createTextNode("Message: "+mails[i].children[3].innerHTML);
+                document.getElementById("mail-box").appendChild(message);
+                message.appendChild(messageValue);
+            }
         }
     };
 
     xhttp.open("GET", "./mail.xml", true);
     xhttp.send();
 }
+
+// setInterval(() =>{
+// },10);
