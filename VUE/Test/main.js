ea723508d48464affd5681d591f739bcb2906946
unknown <victorst79@gmail.com>
2019-01-08 14:33:36 +0100

    add vue repository

---
+++ VUE/Test/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: false
+    }
+});
