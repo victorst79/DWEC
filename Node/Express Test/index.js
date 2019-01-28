79fb69df1ce3b3198f5337d19d1a6077b6461988
unknown <victorst79@gmail.com>
2019-01-28 18:06:45 +0100

    add express and node folder in repository

---
+++ Node/Express Test/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('NaM!'))
+app.get('/man', (req, res) => res.send('TEST NAM!'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
