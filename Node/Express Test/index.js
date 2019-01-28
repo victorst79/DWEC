d1c00851d3abd57c861b0f83e6fd875ddd1f72fc
unknown <victorst79@gmail.com>
2019-01-28 19:07:07 +0100

    working in express example

--- Node/Express Test/index.js
+++ Node/Express Test/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('NaM!'))
+// app.get('/', (req, res) => res.send('NaM!'))
 app.get('/man', (req, res) => res.send('TEST NAM!'))
 
 app.use(express.static(__dirname + '/public'))
