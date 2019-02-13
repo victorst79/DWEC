6f99baed709b5074cf06733249608aa91c6da741
unknown <victorst79@gmail.com>
2019-02-13 12:47:49 +0100

    api petition included

--- Angular/test-app/src/app/app.module.ts
+++ Angular/test-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { TestComponentComponent } from './test-component/test-component.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/test-app/src/app/test-component/test-component.component.ts
+++ Angular/test-app/src/app/test-component/test-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-test-component',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-component.component.css']
 })
 export class TestComponentComponent implements OnInit {
+  public todos = [1, 2, 3, 4, 5];
+  public testLabel;
+  public title = 'title';
+  public testSwitch = 1;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.http.get('https://fakerestapi.azurewebsites.net/api/Books').subscribe(data => {
+      console.log(data);
+    });
+  }
 
   ngOnInit() {
   }
 
+  testFunction() {
+    alert('WORKING');
+  }
 }
